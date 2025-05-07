ff28a528e3323d2c353444bdd78374eaecd8e3fd
Şêrwan <96183043+Ausuyyn@users.noreply.github.com>
2025-05-07 12:47:33 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Emiii', // Your name
   ownerNumber: '59893093652', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
